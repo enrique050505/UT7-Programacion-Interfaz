12cca3c304824042a2325da4496d56077379b155
enrique050505 <enriquemartinalbodo@gmail.com>
2024-06-01 11:52:21 +0200

    cambio algunos colores de la interfaz

--- src/main/java/proyecto/InterfazApp.java
+++ src/main/java/proyecto/InterfazApp.java
@@ -90,20 +90,21 @@ public class InterfazApp extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setBounds(100, 100, 1421, 900);
         contentPane = new JPanel();
-        contentPane.setBackground(new Color(255, 255, 255));
+        contentPane.setBackground(new Color(230, 230, 250));
         contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
         setContentPane(contentPane);
         contentPane.setLayout(null);
+        lblCantidadMonedas.setBackground(new Color(255, 255, 255));
         lblCantidadMonedas.setHorizontalAlignment(SwingConstants.CENTER);
 
         lblCantidadMonedas.setFont(new Font("Tahoma", Font.BOLD, 30));
-        lblCantidadMonedas.setForeground(new Color(0, 0, 0));
-        lblCantidadMonedas.setBounds(52, 264, 70, 40);
+        lblCantidadMonedas.setForeground(new Color(184, 134, 11));
+        lblCantidadMonedas.setBounds(43, 269, 70, 40);
         contentPane.add(lblCantidadMonedas);
         
         JPanel panelSeleccionPokemons = new JPanel();
-        panelSeleccionPokemons.setBackground(Color.WHITE);
+        panelSeleccionPokemons.setBackground(new Color(255, 235, 205));
         panelSeleccionPokemons.setBounds(53, 330, 551, 459);
         contentPane.add(panelSeleccionPokemons);
         panelSeleccionPokemons.setLayout(null);
@@ -123,7 +124,7 @@ public class InterfazApp extends JFrame {
         panelSeleccionPokemons.add(btnPokemon1);
         
         JButton btnPokemon2 = new JButton();
-        btnPokemon2.setBackground(new Color(102, 204, 255));
+        btnPokemon2.setBackground(new Color(0, 191, 255));
         btnPokemon2.setForeground(new Color(0, 0, 0));
         btnPokemon2.addActionListener(new ActionListener() {
         	public void actionPerformed(ActionEvent e) {
@@ -166,7 +167,7 @@ public class InterfazApp extends JFrame {
         chckbx_Pokemon1.setEnabled(false);
         chckbx_Pokemon1.setSelected(false);
         chckbx_Pokemon1.setHorizontalAlignment(SwingConstants.CENTER);
-        chckbx_Pokemon1.setBackground(new Color(255, 255, 255));
+        chckbx_Pokemon1.setBackground(new Color(255, 235, 205));
         chckbx_Pokemon1.setBounds(6, 57, 45, 38);
         panelSeleccionPokemons.add(chckbx_Pokemon1);
         
@@ -174,7 +175,7 @@ public class InterfazApp extends JFrame {
         chckbx_Pokemon2.setEnabled(false);
         chckbx_Pokemon2.setSelected(false);
         chckbx_Pokemon2.setHorizontalAlignment(SwingConstants.CENTER);
-        chckbx_Pokemon2.setBackground(Color.WHITE);
+        chckbx_Pokemon2.setBackground(new Color(255, 235, 205));
         chckbx_Pokemon2.setBounds(6, 213, 45, 38);
         panelSeleccionPokemons.add(chckbx_Pokemon2);
         
@@ -183,7 +184,7 @@ public class InterfazApp extends JFrame {
         chckbx_Pokemon3.setSelected(false);
         chckbx_Pokemon3.setFont(new Font("Tahoma", Font.PLAIN, 10));
         chckbx_Pokemon3.setHorizontalAlignment(SwingConstants.CENTER);
-        chckbx_Pokemon3.setBackground(new Color(255, 255, 255));
+        chckbx_Pokemon3.setBackground(new Color(255, 235, 205));
         chckbx_Pokemon3.setBounds(6, 380, 45, 38);
         panelSeleccionPokemons.add(chckbx_Pokemon3);
         
@@ -192,7 +193,7 @@ public class InterfazApp extends JFrame {
         JButton btnComprarPokemon1 = new JButton("Comprar");
         btnComprarPokemon1.setBounds(340, 20, 138, 38);
         panelSeleccionPokemons.add(btnComprarPokemon1);
-        btnComprarPokemon1.setBackground(new Color(0, 255, 128));
+        btnComprarPokemon1.setBackground(new Color(0, 255, 127));
         btnComprarPokemon1.setForeground(new Color(0, 0, 0));
         btnComprarPokemon1.setFont(new Font("Tahoma", Font.BOLD, 22));
         btnComprarPokemon1.setEnabled(false);
@@ -222,7 +223,7 @@ public class InterfazApp extends JFrame {
         JButton btnComprarPokemon2 = new JButton("Comprar");
         btnComprarPokemon2.setForeground(Color.BLACK);
         btnComprarPokemon2.setFont(new Font("Tahoma", Font.BOLD, 22));
-        btnComprarPokemon2.setBackground(new Color(0, 255, 128));
+        btnComprarPokemon2.setBackground(new Color(0, 255, 127));
         btnComprarPokemon2.setBounds(340, 179, 138, 38);
         btnComprarPokemon2.setEnabled(false);
         panelSeleccionPokemons.add(btnComprarPokemon2);
@@ -253,7 +254,7 @@ public class InterfazApp extends JFrame {
         JButton btnComprarPokemon3 = new JButton("Comprar");
         btnComprarPokemon3.setForeground(Color.BLACK);
         btnComprarPokemon3.setFont(new Font("Tahoma", Font.BOLD, 22));
-        btnComprarPokemon3.setBackground(new Color(0, 255, 128));
+        btnComprarPokemon3.setBackground(new Color(0, 255, 127));
         btnComprarPokemon3.setBounds(340, 340, 138, 38);
         btnComprarPokemon3.setEnabled(false);
         panelSeleccionPokemons.add(btnComprarPokemon3);
@@ -287,7 +288,7 @@ public class InterfazApp extends JFrame {
         contentPane.add(lblPokedex);
 
         JPanel panel_InformacionGeneral = new JPanel();
-        panel_InformacionGeneral.setBackground(Color.WHITE);
+        panel_InformacionGeneral.setBackground(new Color(248, 248, 255));
         panel_InformacionGeneral.setBounds(727, 299, 644, 490);
         contentPane.add(panel_InformacionGeneral);
         panel_InformacionGeneral.setLayout(null);
@@ -315,6 +316,7 @@ public class InterfazApp extends JFrame {
         lblImagenPokemon.setFont(new Font("Arial", Font.BOLD, 13));
         lblImagenPokemon.setBounds(10, 37, 205, 169);
         panel_InformacionGeneral.add(lblImagenPokemon);
+        lblTipos.setForeground(new Color(34, 34, 249));
         lblTipos.setHorizontalAlignment(SwingConstants.CENTER);
         
         lblTipos.setFont(new Font("Arial", Font.BOLD, 20));
@@ -332,7 +334,7 @@ public class InterfazApp extends JFrame {
         panel_InformacionGeneral.add(lblTipo2);
         
         textAreaDescripcion.setForeground(new Color(128, 0, 128));
-        textAreaDescripcion.setBackground(new Color(255, 255, 255));
+        textAreaDescripcion.setBackground(new Color(240, 240, 240));
         textAreaDescripcion.setFont(new Font("Arial", Font.BOLD, 16));
         textAreaDescripcion.setEditable(false); 
         textAreaDescripcion.setLineWrap(true); 
@@ -420,7 +422,7 @@ public class InterfazApp extends JFrame {
         lblMostrarPeso.setBounds(541, 436, 44, 41);
         panel_InformacionGeneral.add(lblMostrarPeso);
         
-        lblText_Estadisticas.setForeground(new Color(0, 128, 0));
+        lblText_Estadisticas.setForeground(new Color(0, 153, 51));
         lblText_Estadisticas.setHorizontalAlignment(SwingConstants.CENTER);
         lblText_Estadisticas.setBounds(215, 223, 176, 26);
         lblText_Estadisticas.setFont(new Font("Verdana", Font.BOLD, 24));
@@ -458,15 +460,17 @@ public class InterfazApp extends JFrame {
         lblIMG_Personaje.setIcon((new ImageIcon(getClass().getClassLoader().getResource(("img/personaje.png")))));
         lblIMG_Personaje.setBounds(10, 10, 733, 150);
         contentPane.add(lblIMG_Personaje);
+        lblImgMonedas.setHorizontalAlignment(SwingConstants.CENTER);
+        lblImgMonedas.setBackground(new Color(255, 255, 128));
         
         lblImgMonedas.setIcon(new ImageIcon(getClass().getClassLoader().getResource(("img/monedas.jpg"))));
-        lblImgMonedas.setBounds(113, 264, 79, 56);
+        lblImgMonedas.setBounds(112, 264, 80, 56);
         contentPane.add(lblImgMonedas);
         
         lblText_InformacionGeneral.setHorizontalAlignment(SwingConstants.CENTER);
         lblText_InformacionGeneral.setForeground(new Color(128, 0, 128));
         lblText_InformacionGeneral.setFont(new Font("Tahoma", Font.BOLD, 30));
-        lblText_InformacionGeneral.setBounds(923, 249, 327, 40);
+        lblText_InformacionGeneral.setBounds(902, 249, 327, 40);
         contentPane.add(lblText_InformacionGeneral);
         lblMensajeAlUsuario.setBackground(new Color(255, 255, 255));
        
@@ -476,18 +480,20 @@ public class InterfazApp extends JFrame {
         contentPane.add(lblMensajeAlUsuario);
         
         //USUARIOS: COMBOBOX Y BOTON
-        JComboBox comboBox_Usuario = new JComboBox();
-        comboBox_Usuario.setFont(new Font("Arial", Font.BOLD, 18));
-        comboBox_Usuario.setModel(new DefaultComboBoxModel(new String[] {"Seleccionar Usuario"}));
-        comboBox_Usuario.setToolTipText("");
-        comboBox_Usuario.setBounds(52, 173, 215, 40);
-        contentPane.add(comboBox_Usuario);
-        rellenarComboBox(comboBox_Usuario);
+        JComboBox JcomboBox_Usuario = new JComboBox();
+        JcomboBox_Usuario.setBackground(new Color(229, 229, 229));
+        JcomboBox_Usuario.setFont(new Font("Arial", Font.BOLD, 18));
+        JcomboBox_Usuario.setModel(new DefaultComboBoxModel(new String[] {"Seleccionar Usuario"}));
+        JcomboBox_Usuario.setToolTipText("");
+        JcomboBox_Usuario.setBounds(52, 173, 215, 40);
+        contentPane.add(JcomboBox_Usuario);
+        rellenarComboBox(JcomboBox_Usuario);
         
         JButton btnUsuario = new JButton("Actualizar Usuario");
+        btnUsuario.setBackground(new Color(255, 255, 128));
         btnUsuario.addActionListener(new ActionListener() {
         	public void actionPerformed(ActionEvent e) {
-        		String usuarioSeleccionado =  (String) comboBox_Usuario.getSelectedItem();
+        		String usuarioSeleccionado =  (String) JcomboBox_Usuario.getSelectedItem();
         		if(!usuarioSeleccionado.equals("Seleccionar Usuario")) {
         			ConectorMongo nuevoConectorMongo = new ConectorMongo();
         			nuevoConectorMongo.obtenerInfoUsuario(usuarioSeleccionado, lblCantidadMonedas);
@@ -516,15 +522,16 @@ public class InterfazApp extends JFrame {
         btnUsuario.setFont(new Font("Verdana", Font.BOLD, 22));
         btnUsuario.setBounds(277, 170, 280, 41);
         contentPane.add(btnUsuario);
+        lblIMG_Pokemon.setForeground(new Color(255, 255, 255));
 
-        lblIMG_Pokemon.setBounds(795, 24, 588, 199);
+        lblIMG_Pokemon.setBounds(763, 24, 600, 199);
         contentPane.add(lblIMG_Pokemon);
         lblIMG_Pokemon.setIcon(new ImageIcon(getClass().getClassLoader().getResource(("img/Pokemon-Logo.jpg"))));
         
         JSeparator separator_1 = new JSeparator();
         separator_1.setOrientation(SwingConstants.VERTICAL);
         separator_1.setBackground(new Color(0, 0, 0));
-        separator_1.setForeground(new Color(0, 0, 0));
+        separator_1.setForeground(new Color(169, 169, 169));
         separator_1.setBounds(658, 330, 2, 459);
         contentPane.add(separator_1);
     }//FIN INTERFAZ
